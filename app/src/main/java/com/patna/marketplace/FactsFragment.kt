a62283371c4c5e0be9d3380f6b10280baa511810
Vikas Jha <vikasjhag@gmail.com>
2021-09-10 21:22:00 +0530

    recycler view added

--- app/src/main/java/com/patna/marketplace/FactsFragment.kt
+++ app/src/main/java/com/patna/marketplace/FactsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.patna.marketplace.databinding.FragmentFactsBinding
+import com.patna.marketplace.model.FactAdapter
 import com.patna.marketplace.model.MarketPlaceDatabase
 
 class FactsFragment : Fragment() {
@@ -35,8 +36,13 @@ class FactsFragment : Fragment() {
         binding.factsViewModel = factsViewModel
         binding.setLifecycleOwner(viewLifecycleOwner)
 
+        val adapter = FactAdapter()
+        binding.factList.adapter = adapter
+
         factsViewModel.facts.observe(viewLifecycleOwner,{
-            binding.factsDetailTv.setText(it.toString())
+           it?.let {
+               adapter.data = it
+           }
         })
 
         Toast.makeText(context,args.categoryType.name,Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/patna/marketplace/model/FactAdapter.kt
@@ -0,0 +1,49 @@
+package com.patna.marketplace.model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.patna.marketplace.R
+
+class FactAdapter: RecyclerView.Adapter<FactAdapter.FactViewHolder>() {
+
+
+    var data = listOf<Fact>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FactViewHolder {
+       val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.fact_list_item,parent,false)
+        return FactViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: FactViewHolder, position: Int) {
+        val fact = data.get(position)
+        holder.bindData(fact)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class FactViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+
+        private val headingView:TextView
+        private val bodyView:TextView
+        init {
+            headingView = itemView.findViewById(R.id.heading_tv)
+            bodyView = itemView.findViewById(R.id.body_tv)
+        }
+        fun bindData(fact: Fact){
+            headingView.setText(fact.heading)
+            bodyView.setText(fact.body)
+        }
+    }
+}
